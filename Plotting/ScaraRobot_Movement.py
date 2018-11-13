ae81c3f55132ad995d7e22dc946f2cee5f6078cb
Marco Jordi <jordi.marco@bluewin.ch>
2018-11-13 08:55:06 +0100

    Add scara robot animation

--- Plotting/ScaraRobot_Movement.py
+++ Plotting/ScaraRobot_Movement.py
@@ -7,6 +7,7 @@ import traceback
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("../plot_style.mplstyle")
+from matplotlib.animation import FuncAnimation, writers
 ###############################################################################
 #
 # Header
@@ -22,7 +23,7 @@ Created on Fri Nov  9 13:43:40 2018
 """
 ###############################################################################
 ###############################################################################
-   
+        
 if __name__ == "__main__":
 
     try:
@@ -36,9 +37,10 @@ if __name__ == "__main__":
         phi2 = 150      #Eingabe des halben Bewegungswinkel in Grad
         
         #Bewegungswinkel als Vektoren
-        p1 = np.linspace((-phi1/180)*np.pi, (phi1/180)*np.pi, 1000)
-        p2 = np.linspace(0, (phi2/180)*np.pi, 1000)
-        p22 = np.linspace(-(phi2/180)*np.pi, (phi2/180)*np.pi, 1000)
+        p1 = np.linspace((-phi1/180)*np.pi, (phi1/180)*np.pi, 100)
+        p2 = np.linspace(0, (phi2/180)*np.pi, 50)
+        p11 = np.linspace((-phi1/180)*np.pi, (phi1/180)*np.pi, 250)
+        p22 = np.linspace(-(phi2/180)*np.pi, (phi2/180)*np.pi, 250)
         cross = np.linspace(-(r1+r2),(r1+r2))
         
         #Bewegungsgleichungen mit Fallunterscheidng
@@ -55,23 +57,59 @@ if __name__ == "__main__":
         #Gesamter Arbeitsbereich
         xx = []
         yy = []
-        for p1i in p1:
+        for p1i in p11:
             for p2i in p22:
                 xx.append(r1 * np.cos(p1i) + r2 * np.cos(p1i + p2i))
                 yy.append(r1 * np.sin(p1i) + r2 * np.sin(p1i + p2i))
         
         #Plot figure
-        plt.figure(figsize=(8,6))
-        plt.plot(x1,y1,x2,y2,x3,y3, lw=3)
-        plt.plot(xx, yy,alpha=0.3)
-        plt.plot(cross, 0*cross, lw=0.5, color="black")
-        plt.plot(0*cross, cross, lw=0.5, color="black")
-        plt.legend(["Fall 1","Fall 2","Fall 3","Gesamt",])
-        plt.title("Scara Roboter Arbeitsbereich")
-        plt.xlabel("X-Achse")
-        plt.xlabel("Y-Achse")
-        plt.axis('equal')
-    
+        fig, ax = plt.subplots(figsize=(9,7))
+        ax.plot(x1,y1,x2,y2,x3,y3, lw=2)
+        ax.plot(xx, yy,alpha=0.15)
+        ax.plot(cross, 0*cross, lw=0.5, color="black")
+        ax.plot(0*cross, cross, lw=0.5, color="black")
+        ax.legend(["Fall 1","Fall 2","Fall 3"])
+        ax.set_title("Scara Roboter Arbeitsbereich")
+        ax.set_xlabel("X-Achse")
+        ax.set_ylabel("Y-Achse")
+        ax.axis('equal')
+        ax.set_xlim(-650, 650)
+        ax.set_ylim(-650, 650)
+        
+        #######################################################################
+        #Animation
+        ln, = ax.plot([], [], 'yellowgreen', lw=6, animated=True)
+        lm, = ax.plot([], [], 'green', lw=6, animated=True)
+        
+        #Define update function
+        def update(frame):
+            if frame <= 50:
+                x = [r1 * np.cos(min(p1)), r1 * np.cos(min(p1)) + r2 * np.cos(min(p1)-p2[::-1][frame])]
+                y = [r1 * np.sin(min(p1)), r1 * np.sin(min(p1)) + r2 * np.sin(min(p1)-p2[::-1][frame])]
+                xx = [0, r1 * np.cos(min(p1))]
+                yy = [0, r1 * np.sin(min(p1))]
+                ln.set_data(x, y)
+                lm.set_data(xx, yy)
+            elif 50 < frame <= 150 :
+                x = [r1 * np.cos(p1[frame-50]), r1 * np.cos(p1[frame-50]) + r2 * np.cos(p1[frame-50])]
+                y = [r1 * np.sin(p1[frame-50]), r1 * np.sin(p1[frame-50]) + r2 * np.sin(p1[frame-50])]
+                xx = [0, r1 * np.cos(p1[frame-50])]
+                yy = [0, r1 * np.sin(p1[frame-50])]
+                ln.set_data(x, y)
+                lm.set_data(xx, yy)
+            elif frame > 150:
+                x = [r1 * np.cos(max(p1)), r1 * np.cos(max(p1)) + r2 * np.cos(max(p1)+p2[frame-150])]
+                y = [r1 * np.sin(max(p1)), r1 * np.sin(max(p1)) + r2 * np.sin(max(p1)+p2[frame-150])]
+                xx = [0, r1 * np.cos(max(p1))]
+                yy = [0, r1 * np.sin(max(p1))]
+                ln.set_data(x, y)
+                lm.set_data(xx, yy)
+            return ln, lm,
+        
+        ani = FuncAnimation(fig, update, frames=200, blit=True)
+        
+#        #Save figure
+#        ani.save('animation.gif', writer='pillow', fps=1)
     
     except:
         traceback.print_exc()
